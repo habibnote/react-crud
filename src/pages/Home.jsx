29c0fabef6bed779221ceaffe89d8a5eafd4287f
Habibur Rahman <habibblog97@gmail.com>
2024-08-07 21:51:15 +0600

    misc

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='d-flex justify-content-center align-items-center bg-light vh-100'>
+      <h1>List of users</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Read.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Read() {
+  return (
+    <div className='primary'>Read</div>
+  )
+}
+
+export default Read
